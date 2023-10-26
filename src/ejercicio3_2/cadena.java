a824e37b45eb368cc38209ed73967535935ba8af
LabA1_PC21 <marisolmendoz596@gmail.com>
2023-10-26 11:37:39 -0400

    proyectos

---
+++ src/ejercicio3_2/cadena.java
@@ -0,0 +1,8 @@
+package ejercicio3_2;
+
+public class cadena {
+        String cadenaInicial;
+        public cadena(int cadenaInicial){
+            this.cadenaInicial= String.valueOf(cadenaInicial);
+        }
+   }
